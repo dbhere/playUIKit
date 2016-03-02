6a7fae4017268cef77f1259019c814951fa6008c
alexscottzs <alexzhangshuo@gmail.com>
2016-03-02 17:55:48 +0800

    feat: add Meme object

---
+++ imagePlayGround/Meme.swift
@@ -0,0 +1,17 @@
+//
+//  Meme.swift
+//  imagePlayGround
+//
+//  Created by HhhotDog on 16/3/2.
+//  Copyright © 2016年 Alexscott. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme{
+    var topText:String!
+    var bottomText:String!
+    var image:UIImage!
+    var memedImage:UIImage!
+}

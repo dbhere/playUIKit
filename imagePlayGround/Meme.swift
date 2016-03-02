9772ad478694d557dbea0acf930e3ab2c5fa3489
alexscottzs <alexzhangshuo@gmail.com>
2016-03-02 18:10:10 +0800

    fix:更改了meme object的属性类型

--- imagePlayGround/Meme.swift
+++ imagePlayGround/Meme.swift
@@ -10,12 +10,12 @@ import Foundation
 import UIKit
 
 struct Meme{
-    var topText:String!
-    var bottomText:String!
-    var image:UIImage!
-    var memedImage:UIImage!
+    var topText:String?
+    var bottomText:String?
+    var image:UIImage?
+    var memedImage:UIImage?
     
-    init(topText:String, bottomText:String, image:UIImage, memedImage:UIImage){
+    init(topText:String?, bottomText:String?, image:UIImage?, memedImage:UIImage?){
         self.topText = topText
         self.bottomText = bottomText
         self.image = image

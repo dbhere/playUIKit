a467150ba4d97612a7cb5526d01eb8c21fc9d573
alexscottzs <alexzhangshuo@gmail.com>
2016-03-17 23:25:21 +0800

    feat:完成tableViewController表格呈现

--- SentMemeTableViewController.swift
+++ SentMemeTableViewController.swift
@@ -10,4 +10,27 @@ import UIKit
 
 class SentMemeTableViewController:UITableViewController{
     
+    
+    
+    let delegate = UIApplication.sharedApplication().delegate as! AppDelegate
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return delegate.memes.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("memeCell")!
+        let meme = delegate.memes[indexPath.row]
+        cell.imageView?.image = meme.image
+        cell.textLabel?.text = meme.topText! + meme.bottomText!
+        return cell
+    }
+    
+    @IBAction func createAnMeme(sender: AnyObject) {
+        let VC = self.storyboard!.instantiateViewControllerWithIdentifier("memeViewController") as! ViewController
+        presentViewController(VC, animated: true, completion: nil)
+    }
+    
+    
+    
 }
